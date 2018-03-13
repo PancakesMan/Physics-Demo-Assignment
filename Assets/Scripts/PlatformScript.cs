64a1c8f0ffa36ad9e529fc7642c5f3fd3ad09fff
ProProgrammerEric <eric_work@y7mail.com>
2018-03-13 15:55:40 +1030

    bad bad platform

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+    private ConfigurableJoint joint;
+
+	// Use this for initialization
+	void Start () {
+        joint = GetComponent<ConfigurableJoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        Vector3 old = joint.targetPosition;
+        if (collision.rigidbody.tag == "StillPlatform")
+            joint.targetPosition = new Vector3(-old.x, old.y, old.z);
+    }
+}
